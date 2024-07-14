ac204a206e1dbc71d9480057c977bfde72a65489
Luis Antunes <luisantunes0914@gmail.com>
2024-07-13 22:44:25 -0300

    add dark theme

--- src/main.py
+++ src/main.py
@@ -1,30 +1,49 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+import numpy as np
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout
+import pyqtgraph as pg
+from pyqtgraph.Qt.QtWidgets import QPushButton
+from gui.widgets.styles import setupTheme
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Aplicação PyQt6 Básica")
+        self.setWindowTitle("Exemplo PyQtGraph com PySide6")
 
-        # Criação de um widget central
+        # Criar um widget central
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        # Layout
-        layout = QVBoxLayout()
+        # Botao genérico
+        botao = QPushButton("dsadksa")
 
-        # Adicionando um rótulo
-        label = QLabel("Olá, PyQt6!")
-        layout.addWidget(label)
+        # Layout vertical
+        layout = QGridLayout(central_widget)
 
-        # Aplicando o layout ao widget central
-        central_widget.setLayout(layout)
+        # Criar um widget de gráfico
+        self.plot_widget = pg.PlotWidget()
+        layout.addWidget(self.plot_widget, 1, 1, 5, 5)  # type: ignore
+        layout.addWidget(botao, 6, 2)  # type: ignore
+
+        # Gerar dados
+        x = np.linspace(0, 10, 100)
+        y = np.cosh(x)  # Função seno
+
+        # Adicionar uma curva ao gráfico
+        self.plot_widget.plot(x, y, pen="r", label="Seno")
+
+        # Adicionar uma legenda
+        self.plot_widget.addLegend()
 
 
 # Inicialização da aplicação
 if __name__ == "__main__":
+    # Cria a aplicação
     app = QApplication(sys.argv)
+    setupTheme(app)
     window = MainWindow()
+
+    # Executa tudo
     window.show()
-    sys.exit(app.exec())
+    app.exec()
